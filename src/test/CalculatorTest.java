8081769df074df96f263678d56bf36f656444a2a
Pablo Bedoya <pablo.reds@hotmail.com>
2014-05-26 13:51:25 -0300

    Adding more unit tests #2

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -3,13 +3,20 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.ufrn.gcm.resources.CalculatorMultResource;
 import com.ufrn.gcm.resources.CalculatorSumResource;
 
 public class CalculatorTest {
 	
 	@Test
-	public void testMultiply() {
+	public void testSum() {
 		CalculatorSumResource test = new CalculatorSumResource();
 		assertEquals("15.0", test.sum(10, 5));
+	}
+	
+	@Test
+	public void testMultiply() {
+		CalculatorMultResource test = new CalculatorMultResource();
+		assertEquals("50.0", test.mult(10, 5));
 	}
 }
--- src/test/resources/CalculatorTest.java
+++ src/test/resources/CalculatorTest.java
@@ -1,6 +1,8 @@
 package test.resources;
 
 import static org.junit.Assert.assertEquals;
+import main.resources.com.ufrn.gcm.resources.CalculatorDivResource;
+import main.resources.com.ufrn.gcm.resources.CalculatorMinusResource;
 import main.resources.com.ufrn.gcm.resources.CalculatorMultResource;
 import main.resources.com.ufrn.gcm.resources.CalculatorSumResource;
 
@@ -19,4 +21,17 @@ public class CalculatorTest {
 		CalculatorMultResource test = new CalculatorMultResource();
 		assertEquals("50.0", test.mult(10, 5));
 	}
+	
+	@Test
+	public void testMinus() {
+		CalculatorMinusResource test = new CalculatorMinusResource();
+		assertEquals("5.0", test.minus(10, 5));
+	}
+	
+	@Test
+	public void testDiv() throws Exception {
+		CalculatorDivResource test = new CalculatorDivResource();
+		assertEquals("2.0", test.div(10, 5));
+	}
+	
 }
